71fc6dc54e59c56b35821d3301d7b1f7a3dafdd7
Jonas Nunes <jonasctps@gmail.com>
2022-06-10 11:51:10 -0300

    Banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from './styles'
+export function Banner() {
+  return(
+    <Container>
+      <img src="" alt="" />
+    </Container>
+  )
+}
---
+++ src/components/Banner/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import banner from '../../assets/banner.svg'
+
+
+export const Container = styled.div`
+  background-image: url(${banner});
+  background-size: cover;
+  background-position: center ;
+  width: 100vw;
+  height: 15rem;
+`
